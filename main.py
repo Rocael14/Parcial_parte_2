e08def1c6db56bab1b4b0060d8a58b9e1bdf1bb6
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-09-10 10:41:41 -0600

    Se crea el agregar producto aun falta que verifique que no se repita id

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Producto:
-    def __init__(self, id_producto,nombre, cantidad):
+    def __init__(self, id_producto,nombre, precio,cantidad):
         self.id_producto=id_producto
         self.nombre = nombre
+        self.precio = precio
         self.cantidad = cantidad
 
 
@@ -19,6 +20,27 @@ class Guardar:
     def guardar(self):
         print("Guardando...")
 
+
+class Agregar:
+    def agregar(self):
+        print("Agregando...")
+
+class AgregarProducto(Agregar):
+
+    def agregar(self):
+        diccionario_producto = {}
+        print("--- Registro de Productos ---")
+        id_producto = input("Ingresa el id producto: ")
+        nombre_producto = input("Ingresa el nombre del producto: ")
+        precio_producto = float(input("Ingresa el precio del producto: Q"))
+        cantidad = int(input("Ingresa el cantidad del producto: "))
+        producto = Producto(id_producto, nombre_producto, precio_producto, cantidad)
+        diccionario_producto[id_producto] = {producto}
+
+
+
+
+
 def Menu():
     print("--- Menu ---")
     print("1. Agregar Productos")
@@ -26,5 +48,21 @@ def Menu():
     print("4. Realizar Venta")
     print("5. Salir")
 
+agregar_producto = AgregarProducto()
 while True:
     Menu()
+    try:
+        opciones = int(input("Seleccione una opcion"))
+        if opciones == 1:
+            agregar_producto.agregar()
+        elif opciones == 2:
+            print("Lista de Productos")
+        elif opciones ==3:
+            print("Realizar Venta")
+        elif opciones == 4:
+            print("Gracias por utilizar el programa")
+            break
+        else:
+            print("Opcion no valida")
+    except ValueError:
+        print("Solo es permitido el ingreso numerico")
